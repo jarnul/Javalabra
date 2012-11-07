6147988be98e74cc52c4802f40e79997bae60ddc
jarno <jarno@jarno-desktop.(none)>
2012-11-07 13:19:48 +0200

    Koodin putstausta

--- Javalabra/src/gameBlock.java
+++ Javalabra/src/gameBlock.java
@@ -8,85 +8,83 @@
  * @author jarno
  */
 public class gameBlock {
+
     private int blockType; //0=line-block etc.
     private int[][] blockStructure;
     private int xCoordinate;
     private int yCoordinate;
     private int rotationDegree;
-    
+
     public gameBlock() {
         this(1);
     }
-    
+
     public gameBlock(int type) {
-        this.blockType=type;
-        this.blockStructure=buildBlock(type);
-        this.xCoordinate=0;
-        this.yCoordinate=0;
-        this.rotationDegree=0;
+        this.blockType = type;
+        this.blockStructure = buildBlock(type);
+        this.xCoordinate = 0;
+        this.yCoordinate = 0;
+        this.rotationDegree = 0;
     }
-    
+
     /*
      * Methods to create different blocks
      */
-    
-    private int[][] createLineBlock(){
-         int[][] temp = new int[1][4];
-         for (int i=0;i<4;++i){
-            temp[0][i]=2;
-         }
-         return temp;
+    private int[][] createLineBlock() {
+        int[][] temp = new int[1][4];
+        for (int i = 0; i < 4; ++i) {
+            temp[0][i] = 2;
+        }
+        return temp;
     }
-    
-    private int[][] createSquidBlock(){
-         int[][] temp = new int[4][4];
-         fillTable(temp);
-         temp[0][0]=2;
-         for (int i=0;i<temp[1].length;++i){
-              temp[1][i]=2;
-         }
-         return temp;
+
+    private int[][] createSquidBlock() {
+        int[][] temp = new int[4][4];
+        fillTable(temp);
+        temp[0][0] = 2;
+        for (int i = 0; i < temp[1].length; ++i) {
+            temp[1][i] = 2;
+        }
+        return temp;
     }
-    
-    private int[][] createSquareBlock(){
+
+    private int[][] createSquareBlock() {
         int[][] temp = new int[2][2];
         fillTable(temp);
-        for (int i=0;i<2;++i){
-            for (int j=0;j<2;++j){
-                   temp[i][j]=2;
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 0; j < 2; ++j) {
+                temp[i][j] = 2;
             }
         }
         return temp;
     }
-    
-    private int[][] createPointyBlock(){
+
+    private int[][] createPointyBlock() {
         int[][] temp = new int[3][2];
         fillTable(temp);
-        temp[1][0]=2;
-        for(int i=0;i<3;++i){
-            temp[i][1]=2;
+        temp[1][0] = 2;
+        for (int i = 0; i < 3; ++i) {
+            temp[i][1] = 2;
         }
         return temp;
     }
-    
-    private int[][] createPointyDownBlock(){
+
+    private int[][] createPointyDownBlock() {
         int[][] temp = new int[3][2];
         fillTable(temp);
-        temp[1][1]=2;
-        for (int i=0;i<3;++i){
-            temp[i][0]=2;
+        temp[1][1] = 2;
+        for (int i = 0; i < 3; ++i) {
+            temp[i][0] = 2;
         }
         return temp;
     }
-    
-    
-     /*
+
+    /*
      * Method for generating different kinds of blocks, block * (-1) is the rotated block of a given block
      */
-    
-    private int[][] buildBlock(int type){
+    private int[][] buildBlock(int type) {
         switch (type) {
-        
+
             case 1: {
                 return createLineBlock();
             }
@@ -108,116 +106,119 @@ public class gameBlock {
             }
         }
     }
-    
+
     /*
      * Method to fill a int[][]-table with 0's
      */
-    
-    private void fillTable(int[][] temp){
-        for (int i=0;i<temp.length;++i){
-            for (int j=0;j<temp[i].length;++j){
-                temp[i][j]=0;
+    private void fillTable(int[][] temp) {
+        for (int i = 0; i < temp.length; ++i) {
+            for (int j = 0; j < temp[i].length; ++j) {
+                temp[i][j] = 0;
             }
         }
     }
-    
+
     /*
      * Method for regular Matrix Transpose
      */
-    
-    private int[][] matrixTranspose(int[][] temp){
-         int[][] rotated = new int[temp[0].length][temp.length];
-         for (int i=0;i<rotated.length;++i){
-                for (int j=0;j<rotated[i].length;++j){
-                    rotated[i][j]=temp[j][i];
-                }
-         }
-         return rotated;
+    private int[][] matrixTranspose(int[][] temp) {
+        int[][] rotated = new int[temp[0].length][temp.length];
+        for (int i = 0; i < rotated.length; ++i) {
+            for (int j = 0; j < rotated[i].length; ++j) {
+                rotated[i][j] = temp[j][i];
+            }
+        }
+        return rotated;
     }
-    
+
     /*
      * Method for Matrix transpose about different origin
      */
-    
-    private int[][] matrixTransposeSkewed(int[][] temp){
+    private int[][] matrixTransposeSkewed(int[][] temp) {
         int[][] rotated = new int[temp[0].length][temp.length];
-        for (int i=0;i<rotated.length;++i){
-                for (int j=0;j<rotated[i].length;++j){
-                    rotated[rotated.length-i-1][rotated[i].length-j-1]=this.blockStructure[j][i];
-                }
+        for (int i = 0; i < rotated.length; ++i) {
+            for (int j = 0; j < rotated[i].length; ++j) {
+                rotated[rotated.length - i - 1][rotated[i].length - j - 1] = this.blockStructure[j][i];
             }
+        }
         return rotated;
     }
-    
+
     /*
-     * Method to rotate pieces
+     * Methods to fix rotation degrees for special blocks
      */
     
-    public void rotate(){
+    private void rotationDegreeEven() {
+        if (this.blockType == 4) {
+            if (this.rotationDegree == 2) {
+                ++this.xCoordinate;
+                this.rotationDegree = 3;
+            } else {
+                this.rotationDegree = 1;
+            }
+
+        } else if (this.blockType == 2) {
+            this.rotationDegree = 1;
+        }
+    }
+    
+    private void rotationDegreeOdd() {
+        if (this.blockType == 4) {
+            if (this.rotationDegree == 1) {
+                ++this.yCoordinate;
+                this.rotationDegree = 2;
+            } else {
+                --this.xCoordinate;
+                this.rotationDegree = 0;
+            }
+        } else if (this.blockType == 2) {
+            this.rotationDegree = 0;
+        }
+    }
+
+    /*
+     * Method to rotate pieces using matrix-transposes
+     */
+    public void rotate() {
         //Creates the transpose-matrix of this.blockStructure
         int[][] rotated;
         //rotationDegree is used in case block must be transposed from different origins (as with pointyBlock for example)
-        if (this.rotationDegree==0 || this.rotationDegree==2){
-            rotated=matrixTranspose(this.blockStructure);
-            if (this.blockType==4){
-                 if (this.rotationDegree==2){
-                     ++this.xCoordinate;
-                     this.rotationDegree=3;
-                 }
-                 else {
-                    this.rotationDegree=1;   
-                 }
+        if (this.rotationDegree == 0 || this.rotationDegree == 2) {
+            rotated = matrixTranspose(this.blockStructure);
+            //Special cases for pointyblocks and squidblocks
+            rotationDegreeEven();
 
-            }
-            else if (this.blockType==2){
-                this.rotationDegree=1;
-            }
+        } else {
+            rotated = matrixTransposeSkewed(this.blockStructure);
+            rotationDegreeOdd();
         }
-        
-        else {
-            rotated=matrixTransposeSkewed(this.blockStructure);
-            if (this.blockType==4){
-                if(this.rotationDegree==1){
-                    ++this.yCoordinate;
-                    this.rotationDegree=2;
-                }
-                else{
-                    --this.xCoordinate;
-                    this.rotationDegree=0;
-                }
-            }
-            else if (this.blockType==2){
-                this.rotationDegree=0;
-            }
-            
-        }
-        this.blockStructure=rotated;
+        this.blockStructure = rotated;
     }
-    
-    public int getBlockType(){
+
+    public int getBlockType() {
         return this.blockType;
     }
-    
-    public int getBlockXco(){
+
+    public int getBlockXco() {
         return this.xCoordinate;
     }
-    
-    public int getBlockYco(){
+
+    public int getBlockYco() {
         return this.yCoordinate;
     }
-    
+
     public void setBlockXco(int x) {
-        this.xCoordinate=x;
+        this.xCoordinate = x;
     }
-    
+
     public void setBlockYco(int y) {
-        this.yCoordinate=y;
+        this.yCoordinate = y;
     }
-    
-    public void addBlockYco(){
+
+    public void addBlockYco() {
         ++this.yCoordinate;
     }
-    
+
     public int[][] getBlockStructure() {
         return this.blockStructure;
     }
--- Javalabra/src/gameLogic.java
+++ Javalabra/src/gameLogic.java
@@ -8,9 +8,9 @@
  * @author jarno
  */
 public class gameLogic {
-    
+
     //Status-table for game, for every x,y co-ordinate the table has a int-value between 0 and 4,
-    //0 is empty square, 1 is black square, 2 is black square that is falling down and 3 is moving square that can't fall anymore,
+    //0 is empty square, 1 is black square and 2 is black square that is falling down
     //and it will be changed to 1.
     private int[][] status;
     //width and heigth of the tetris-game
@@ -20,90 +20,81 @@ public class gameLogic {
     private boolean movingBlocks;
     private gameBlock currentBlock;
 
-    
     public gameLogic() {
-        this.width=12;
-        this.height=15;
+        this.width = 12;
+        this.height = 15;
         //fill up this.status and this.rotations
         this.status = new int[this.width][this.height];
         fillTables(this.status);
-        this.movingBlocks=false;
-        
+        this.movingBlocks = false;
+
     }
 
     /*
      * Method to generate random block
      */
-    
     private gameBlock generateBlock() {
         //code to generate random block
-        return new gameBlock(2);
+        return new gameBlock(4);
     }
-        
+
     /*
      * Method for filling table
      */
-    
     private void fillTables(int[][] temp) {
-        for (int i=0;i<temp.length;++i){
-            for (int j=0;j<temp[i].length;++j){
-                temp[i][j]=0;
+        for (int i = 0; i < temp.length; ++i) {
+            for (int j = 0; j < temp[i].length; ++j) {
+                temp[i][j] = 0;
             }
         }
     }
-    
+
     /*
      * Method for filling table
      */
-    
-    private void fillTables(int[][] temp, int oldNumber, int newNumber){
-        for (int i=0;i<temp.length;++i){
-            for(int j=0;j<temp.length;++j){
-                if (temp[i][j]==oldNumber){
-                    temp[i][j]=newNumber;
+    private void fillTables(int[][] temp, int oldNumber, int newNumber) {
+        for (int i = 0; i < temp.length; ++i) {
+            for (int j = 0; j < temp.length; ++j) {
+                if (temp[i][j] == oldNumber) {
+                    temp[i][j] = newNumber;
                 }
             }
         }
     }
-    
+
     /*
      * Method for updating game-status, usually bringing block down for one row
      */
-    
     public void updateGame() {
-        
+
         //Code to generate new block and to put it in game
-        if(!this.movingBlocks){
-            this.currentBlock=generateBlock();
-            int[][] tempBlock=this.currentBlock.getBlockStructure();
-            for (int i=0;i<tempBlock.length;++i){
-                for (int j=0;j<tempBlock[i].length;++j){
-                    if (this.status[i][j]==0){
-                        this.status[i][j]=tempBlock[i][j];
-                    }
-                    else{
+        if (!this.movingBlocks) {
+            this.currentBlock = generateBlock();
+            int[][] tempBlock = this.currentBlock.getBlockStructure();
+            for (int i = 0; i < tempBlock.length; ++i) {
+                for (int j = 0; j < tempBlock[i].length; ++j) {
+                    if (this.status[i][j] == 0) {
+                        this.status[i][j] = tempBlock[i][j];
+                    } else {
                         //Code when the game is lost
                     }
                 }
             }
-            this.movingBlocks=true;
-        }
-        
-        //Code to check if block can be moved
-        else{
-            int[][] tempBlock=this.currentBlock.getBlockStructure();
-            boolean tempBreak=true;
-            for (int i=0;i<tempBlock.length;++i){
-                for (int j=tempBlock[i].length-1;j>-1;--j){
+            this.movingBlocks = true;
+        } //Code to check if block can be moved
+        else {
+            int[][] tempBlock = this.currentBlock.getBlockStructure();
+            boolean tempBreak = true;
+            for (int i = 0; i < tempBlock.length; ++i) {
+                for (int j = tempBlock[i].length - 1; j > -1; --j) {
                     //Checks if block can be moved down by one
-                    if (tempBlock[i][j]==2){
+                    if (tempBlock[i][j] == 2) {
                         //If checks if moved block is in the vertical bounds, done for all the blocks in current moving block
-                        if (j+this.currentBlock.getBlockYco()<this.status[i].length-1 && this.status[this.currentBlock.getBlockXco()+i][this.currentBlock.getBlockYco()+j+1]!=1) {
-                               //Currently analyzed block can be moved down
-                        }
-                        else {
-                            tempBreak=false;
-                            this.movingBlocks=false;
+                        if (j + this.currentBlock.getBlockYco() < this.status[i].length - 1 && this.status[this.currentBlock.getBlockXco() + i][this.currentBlock.getBlockYco() + j + 1] != 1) {
+                            //Currently analyzed block can be moved down
+                        } else {
+                            tempBreak = false;
+                            this.movingBlocks = false;
                             clearMovingBlocks(1);
                             break;
                         }
@@ -111,63 +102,60 @@ public class gameLogic {
                 }
             }
             //If all the squares in the block can be moved down
-            if (tempBreak){
+            if (tempBreak) {
                 moveBlockDown();
             }
         }
     }
-    
+
     /*
      * Method to turn all 2's into given integer
      */
-    
     private void clearMovingBlocks(int filler) {
-        for (int i=0;i<this.status.length;++i){
-            for (int j=0;j<this.status[i].length;++j) {
-                if (this.status[i][j]==2){
-                    this.status[i][j]=filler;
+        for (int i = 0; i < this.status.length; ++i) {
+            for (int j = 0; j < this.status[i].length; ++j) {
+                if (this.status[i][j] == 2) {
+                    this.status[i][j] = filler;
                 }
             }
         }
     }
-    
+
     /*
      * Method to move current block down by one move
      */
-    
     private void moveBlockDown() {
         int[][] temp = this.currentBlock.getBlockStructure();
-        for (int i=0;i<temp.length;++i){
-            for (int j=temp[i].length-1;j>-1;--j){
-                if (temp[i][j]==2){
-                    this.status[i+this.currentBlock.getBlockXco()][j+this.currentBlock.getBlockYco()+1]=2;
-                    this.status[i+this.currentBlock.getBlockXco()][j+this.currentBlock.getBlockYco()]=0;
+        for (int i = 0; i < temp.length; ++i) {
+            for (int j = temp[i].length - 1; j > -1; --j) {
+                if (temp[i][j] == 2) {
+                    this.status[i + this.currentBlock.getBlockXco()][j + this.currentBlock.getBlockYco() + 1] = 2;
+                    this.status[i + this.currentBlock.getBlockXco()][j + this.currentBlock.getBlockYco()] = 0;
                 }
             }
         }
-        this.currentBlock.addBlockYco(); 
+        this.currentBlock.addBlockYco();
     }
 
     /*
      * Method to rotate pieces
      */
-    
     public void rotatePiece() {
         clearMovingBlocks(0);
         this.currentBlock.rotate();
         fillIn(this.currentBlock);
     }
-    
-    private void fillIn(gameBlock temp){
+
+    private void fillIn(gameBlock temp) {
         int[][] tempBlock = temp.getBlockStructure();
-        for (int i=0;i<tempBlock.length;++i){
-            for (int j=0;j<tempBlock[i].length;++j) {
-                this.status[i+temp.getBlockXco()][j+temp.getBlockYco()]=tempBlock[i][j];
+        for (int i = 0; i < tempBlock.length; ++i) {
+            for (int j = 0; j < tempBlock[i].length; ++j) {
+                this.status[i + temp.getBlockXco()][j + temp.getBlockYco()] = tempBlock[i][j];
             }
         }
     }
-    
-    public int[][] getGameStatus(){
+
+    public int[][] getGameStatus() {
         return this.status;
     }
 }
